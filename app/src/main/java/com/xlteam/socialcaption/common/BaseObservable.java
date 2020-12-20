511eedd91ce3b078fbe1bf0896a6ad72bbd8dbfd
RunAnywhere <tientuyen07@gmail.com>
2020-12-20 10:33:29 +0700

    Base for DI

---
+++ app/src/main/java/com/xlteam/socialcaption/common/BaseObservable.java
@@ -0,0 +1,30 @@
+package com.xlteam.socialcaption.common;
+
+import android.content.Context;
+
+import com.xlteam.socialcaption.ui.common.views.BaseViewMvc;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BaseObservable <LISTENER_CLASS> extends BaseViewMvc {
+    // thread-safe set of listeners
+    private final Set<LISTENER_CLASS> mListener = Collections.newSetFromMap(
+            new ConcurrentHashMap<LISTENER_CLASS, Boolean>(1));
+
+    public final void registerListener(LISTENER_CLASS listener) {
+        mListener.add(listener);
+    }
+
+    public final void unregisterListener(LISTENER_CLASS listener) {
+        mListener.remove(listener);
+    }
+
+    protected final Set<LISTENER_CLASS> getListeners(){
+        return Collections.unmodifiableSet(mListener);
+    }
+
+    protected Context getApplicationContext(){
+        return getRootView().getContext();
+    }
+}
---
+++ app/src/main/java/com/xlteam/socialcaption/common/dependencyinjection/ControllerCompositionRoot.java
@@ -0,0 +1,49 @@
+package com.xlteam.socialcaption.common.dependencyinjection;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.xlteam.socialcaption.ui.ViewMvcFactory;
+
+public class ControllerCompositionRoot {
+    private final ActivityCompositionRoot mActivityCompositionRoot;
+
+    public ControllerCompositionRoot(ActivityCompositionRoot activityCompositionRoot) {
+        mActivityCompositionRoot = activityCompositionRoot;
+    }
+
+    public FragmentActivity getActivity() {
+        return mActivityCompositionRoot.getActivity();
+    }
+
+    private Context getContext() {
+        return getActivity();
+    }
+
+
+//    public PlayActivityController getPlayActivityController() {
+//        return new PlayActivityController(getDbController());
+//    }
+//
+//    public MenuActivityController getMenuActivityController() {
+//        return new MenuActivityController();
+//    }
+
+    public ViewMvcFactory getViewMvcFactory() {
+        return new ViewMvcFactory(getLayoutInflater());
+    }
+
+    private LayoutInflater getLayoutInflater() {
+        return LayoutInflater.from(getContext());
+    }
+
+//    private DBController getDbController() {
+//        return DBController.getInstance(getContext());
+//    }
+
+//    public CustomKeyboard getCustomKeyboard() {
+//        return new CustomKeyboard(getActivity());
+//    }
+}
