e2b3bb2a8bf788e0cce7649a6af36aea353e8240
BinhNK <nguyen.khacbinh.bka@gmail.com>
2020-12-21 23:00:03 +0700

    quên return true

--- app/src/main/java/com/xlteam/socialcaption/ui/main/MainActivity.java
+++ app/src/main/java/com/xlteam/socialcaption/ui/main/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                         replaceFragment(R.id.frame, new AccountFragment());
                         break;
                 }
-                return false;
+                return true;
             }
         });
     }
