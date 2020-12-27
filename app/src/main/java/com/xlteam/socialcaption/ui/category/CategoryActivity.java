b592d0102f522804bbc939fa713c7869d0bc986f
BinhNK <nguyen.khacbinh.bka@gmail.com>
2020-12-27 21:29:29 +0700

    tạo CategoryActivity

---
+++ app/src/main/java/com/xlteam/socialcaption/ui/category/CategoryActivity.java
@@ -0,0 +1,16 @@
+package com.xlteam.socialcaption.ui.category;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.xlteam.socialcaption.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
