891b7da5133c5d93087efcbadaec7fb7bdcea523
BinhNK <nguyen.khacbinh.bka@gmail.com>
2020-12-27 17:45:41 +0700

    tạm cái logic

--- app/src/main/java/com/xlteam/socialcaption/common/utility/SharedPreferencesController.java
+++ app/src/main/java/com/xlteam/socialcaption/common/utility/SharedPreferencesController.java
@@ -2,6 +2,8 @@ package com.xlteam.socialcaption.common.utility;
 
 import android.content.Context;
 
+import com.xlteam.socialcaption.model.Category;
+
 import java.util.ArrayList;
 
 public class SharedPreferencesController {
@@ -10,4 +12,11 @@ public class SharedPreferencesController {
         return PrefUtils.getStringArrayList(context, Constant.FIREBASE, Constant.FIREBASE_CATEGORY_LIST);
     }
 
+    public static void putCategoryList(Context context, ArrayList<Category> categoryList) {
+        ArrayList<String> categoryString = new ArrayList<>();
+        for (Category category : categoryList) {
+            categoryString.add(category.getCategoryName());
+        }
+        PrefUtils.putStringArrayList(context, Constant.FIREBASE, Constant.FIREBASE_CATEGORY_LIST, categoryString);
+    }
 }
