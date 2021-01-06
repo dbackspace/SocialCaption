a62956d828672f0af481e5c4a9ea92bfc0e732f4
BinhNK <nguyen.khacbinh.bka@gmail.com>
2021-01-06 23:06:24 +0700

    insert database from firestore

--- app/src/main/java/com/xlteam/socialcaption/firebase/FirebaseController.java
+++ app/src/main/java/com/xlteam/socialcaption/firebase/FirebaseController.java
@@ -1,11 +1,10 @@
 package com.xlteam.socialcaption.firebase;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.xlteam.socialcaption.model.Caption;
-
-import java.util.ArrayList;
-import java.util.Objects;
 
 public class FirebaseController {
     FirebaseFirestore db;
@@ -14,5 +13,17 @@ public class FirebaseController {
         db = FirebaseFirestore.getInstance();
     }
 
+
+    public void updateCaption() {
+        db.collection("captions").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    Caption caption = documentSnapshot.toObject(Caption.class);
+                    //insert caption to database
+                }
+            }
+        });
+    }
 
 }
