6f80825de23754f07c8db29b0d999926ac0eba47
binhnk08 <39753132+binhnk08@users.noreply.github.com>
2021-01-06 11:39:14 +0700

    Update Constant.java

--- app/src/main/java/com/xlteam/socialcaption/common/utility/Constant.java
+++ app/src/main/java/com/xlteam/socialcaption/common/utility/Constant.java
@@ -4,18 +4,4 @@ import com.xlteam.socialcaption.R;
 
 public class Constant {
 
-    public static final String[] CATEGORY_ARRAY = {"Tình yêu", "Cuộc sống", "Thơ ca", "Hài hước", "Bạn bè"};
-    public static final int[] GRADIENT_BACKGROUND_ARRAY = {R.drawable.post_caption_gradient_1, R.drawable.post_caption_gradient_2, R.drawable.post_caption_gradient_3, R.drawable.post_caption_gradient_4, R.drawable.post_caption_gradient_5, R.drawable.post_caption_gradient_6, R.drawable.post_caption_gradient_7, R.drawable.post_caption_gradient_8, R.drawable.post_caption_gradient_9, R.drawable.post_caption_gradient_10, R.drawable.post_caption_gradient_11, R.drawable.post_caption_gradient_12, R.drawable.post_caption_gradient_13, R.drawable.post_caption_gradient_14, R.drawable.post_caption_gradient_15};
-    public static final int[] TOOL_FOR_COLOR_FONT_ALIGN_POST = {R.drawable.ic_baseline_color_lens_24, R.drawable.ic_baseline_font_download_24, R.drawable.ic_baseline_format_color_text_24, R.drawable.ic_baseline_format_align_justify_24};
-    //share preference
-    public static final String FIREBASE = "FIREBASE";
-    public static final String FIREBASE_CATEGORY_LIST = "FIREBASE_CATEGORY_LIST";
-
-    public static final String EXTRA_CATEGORY_NUMBER = "EXTRA_CATEGORY_NUMBER";
-    public static final String EXTRA_TYPE_CATEGORY = "EXTRA_TYPE_CATEGORY";
-    //
-
-    public static final int TYPE_ALL = 0;
-    public static final int TYPE_POST = 1;
-    public static final int TYPE_BOOKMARK = 2;
 }
