9e8bc7d747cf5d1e23579eb6737d3df366a819bf
BinhNK <nguyen.khacbinh.bka@gmail.com>
2021-01-07 22:02:11 +0700

    xml preview và edit

--- app/src/main/java/com/xlteam/socialcaption/ui/MainActivity.java
+++ app/src/main/java/com/xlteam/socialcaption/ui/MainActivity.java
@@ -2,15 +2,16 @@ package com.xlteam.socialcaption.ui;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,8 +20,8 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 import com.xlteam.socialcaption.R;
+import com.xlteam.socialcaption.ui.edit.EditCaptionActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,12 +34,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, EditCaptionActivity.class);
+            startActivity(intent);
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -51,6 +49,33 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    navController.navigate(R.id.nav_home);
+                    drawer.closeDrawer(GravityCompat.START, false);
+                    break;
+                case R.id.nav_gallery:
+                    navController.navigate(R.id.nav_gallery);
+                    drawer.closeDrawer(GravityCompat.START, false);
+                    break;
+                case R.id.nav_rate:
+                    break;
+                case R.id.nav_feedback:
+                    break;
+                case R.id.nav_share:
+                    break;
+                case R.id.nav_other_app:
+                    break;
+                case R.id.nav_term_of_use:
+                    break;
+                case R.id.nav_license:
+                    break;
+            }
+
+            return false;
+        });
     }
 
     @Override
@@ -62,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
         SearchManager searchManager = (SearchManager) MainActivity.this.getSystemService(Context.SEARCH_SERVICE);
 
-        SearchView searchView = null;
+        SearchView searchView;
         if (searchItem != null && searchManager != null) {
             searchView = (SearchView) searchItem.getActionView();
             searchView.setSearchableInfo(searchManager.getSearchableInfo(MainActivity.this.getComponentName()));
--- app/src/main/java/com/xlteam/socialcaption/ui/home/HomeFragment.java
+++ app/src/main/java/com/xlteam/socialcaption/ui/home/HomeFragment.java
@@ -1,22 +1,104 @@
 package com.xlteam.socialcaption.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.tabs.TabLayout;
 import com.xlteam.socialcaption.R;
+import com.xlteam.socialcaption.external.repository.CaptionRepository;
+import com.xlteam.socialcaption.external.repository.ICaptionRepository;
+import com.xlteam.socialcaption.model.Caption;
 
-public class HomeFragment extends Fragment {
+import java.util.List;
 
+import static com.xlteam.socialcaption.external.utility.Constant.LoaderTaskType.LOAD_ALL;
+
+public class HomeFragment extends Fragment implements ICaptionRepository, CaptionAdapter.Callback {
+    private CaptionRepository mRepository;
+    private Context mContext;
+    private RecyclerView rvCaption;
+    private TextView tvNumberCaption;
+    private TabLayout tabLayoutCategory;
+    private TextView tvBookmarkCategory;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        mRepository = new CaptionRepository(mContext, this);
+        tvNumberCaption = root.findViewById(R.id.tv_number_caption);
+        tabLayoutCategory = root.findViewById(R.id.tab_layout_category);
+        tvBookmarkCategory = root.findViewById(R.id.tv_bookmark_select);
+        tabLayoutCategory.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                getCaptionList(tab.getPosition(), tvBookmarkCategory.isActivated());
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        tvBookmarkCategory.setOnClickListener(view1 -> {
+            boolean isBookmark;
+            if (tvBookmarkCategory.isActivated()) {
+                tvBookmarkCategory.setTextColor(mContext.getColor(R.color.color_25));
+                isBookmark = false;
+            } else {
+                tvBookmarkCategory.setTextColor(mContext.getColor(R.color.white));
+                isBookmark = true;
+            }
+            tvBookmarkCategory.setActivated(isBookmark);
+            getCaptionList(tabLayoutCategory.getSelectedTabPosition(), isBookmark);
+        });
+
+        rvCaption = root.findViewById(R.id.rv_caption);
+        rvCaption.setLayoutManager(new LinearLayoutManager(getContext()));
+        getCaptionList(LOAD_ALL, false);
         return root;
+    }
+
+    @Override
+    public void loadResult(int loaderTaskType, List<Caption> result) {
+        if (loaderTaskType == LOAD_ALL) {
+            binCaptions(result);
+        }
+    }
+
+    public void getCaptionList(int categoryNumber, boolean isBookmark) {
+        //lấy database rồi gọi mViewMvc.binCaptions(captions);
+        mRepository.getAllCaption();
+    }
+
+    public void binCaptions(List<Caption> captions) {
+        tvNumberCaption.setText(mContext.getString(R.string.number_captions, captions.size()));
+        CaptionAdapter adapter = new CaptionAdapter(getContext(), captions, this);
+        rvCaption.setAdapter(adapter);
+    }
+
+    @Override
+    public void openCaptionPreview(Caption caption) {
+        PreviewCaptionDialogBuilder previewCaptionDialogBuilder = new PreviewCaptionDialogBuilder(getContext(), caption);
+        previewCaptionDialogBuilder.build().show();
     }
 }
