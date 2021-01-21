ee7a60b95d3ffe3d19023347b5d80f8f87874583
Duc Nguyen <dbackspace.hust@gmail.com>
2021-01-21 17:57:13 +0700

    Add file

--- app/src/main/java/com/xlteam/socialcaption/ui/edit/EditCaptionActivity.java
+++ app/src/main/java/com/xlteam/socialcaption/ui/edit/EditCaptionActivity.java
@@ -1,28 +1,30 @@
 package com.xlteam.socialcaption.ui.edit;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 import com.xlteam.socialcaption.R;
 import com.xlteam.socialcaption.external.repository.UserCaptionRepository;
 import com.xlteam.socialcaption.external.utility.Constant;
 import com.xlteam.socialcaption.model.CommonCaption;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 public class EditCaptionActivity extends AppCompatActivity {
@@ -41,6 +43,9 @@ public class EditCaptionActivity extends AppCompatActivity {
     private int mAlignTextDefault = 0;
     private String mPathImg = "";
     private UserCaptionRepository mRepository;
+
+    private int flipCurrent = 0;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -77,26 +82,38 @@ public class EditCaptionActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int reqCode, int resultCode, Intent data) {
         super.onActivityResult(reqCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-            try {
-                final Uri imageUri = data.getData();
-                mPathImg = imageUri.getPath();
-                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                mImgBackground.setImageBitmap(selectedImage);
-
-                // co su thay doi
-                isPickedPicture = true;
-                enableBtnSave();
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        if (reqCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                mImgBackground.setImageURI(resultUri);
+                mImgBackground.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
             }
 
+       /* if (resultCode == RESULT_OK) {
+
+            } else {
+                try {
+                    final Uri imageUri = data.getData();
+                    mPathImg = imageUri.getPath();
+                    final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    mImgBackground.setImageBitmap(selectedImage);
+
+                    // co su thay doi
+                    isPickedPicture = true;
+                    enableBtnSave();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+            }
         } else {
-            Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+//            Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+        }*/
         }
     }
 
@@ -139,11 +156,31 @@ public class EditCaptionActivity extends AppCompatActivity {
 
 
     public void rotateOnClick(View view) {
+        mImgBackground.setRotation(mImgBackground.getRotation() + -90);
     }
 
     public void cropOnClick(View view) {
+        // start cropping activity for pre-acquired image saved on the device
+        CropImage.activity(getImageUri(getApplicationContext(), getBitmapFromImageView(mImgBackground)))
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
     }
 
     public void flipOnClick(View view) {
+        flipCurrent = (flipCurrent + 180) % 360;
+        mImgBackground.setRotationY(flipCurrent);
+    }
+
+    public Bitmap getBitmapFromImageView(ImageView imageView) {
+        imageView.invalidate();
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        return drawable.getBitmap();
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "temp", null);
+        return Uri.parse(path);
     }
 }
