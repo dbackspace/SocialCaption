0f086278e9a5836ed53cc91fcc9cd877ce794dac
Duc Nguyen <42472693+dbackspace@users.noreply.github.com>
2021-01-31 21:51:01 +0700

    Đảo thứ tự async inflate layout: - fragment_home -> fragment_caption_list: 221ms. - fragment_caption_list -> fragment_home: 198ms.

--- app/src/main/java/com/xlteam/socialcaption/ui/MainActivity.java
+++ app/src/main/java/com/xlteam/socialcaption/ui/MainActivity.java
@@ -100,8 +100,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void asyncLayoutInflate() {
-        int[] layoutList = new int[]{R.layout.fragment_home,
-                R.layout.fragment_caption_list,
+        int[] layoutList = new int[]{R.layout.fragment_caption_list,
+                R.layout.fragment_home,
                 R.layout.fragment_dialog_search,
                 R.layout.fragment_gallery};
         for (int asyncLayoutInflate : layoutList) {
