c0018b319a2f0e35861489c2d75f40613ba18531
binhnk08 <39753132+binhnk08@users.noreply.github.com>
2021-03-15 13:20:09 +0700

    Add files via upload

--- app/src/main/java/com/xlteam/socialcaption/external/utility/utils/Utility.java
+++ app/src/main/java/com/xlteam/socialcaption/external/utility/utils/Utility.java
@@ -1,10 +1,12 @@
 package com.xlteam.socialcaption.external.utility.utils;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -15,6 +17,7 @@ import android.graphics.drawable.ShapeDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Vibrator;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.Animation;
@@ -272,5 +275,28 @@ public class Utility {
         results.add(first + "tam_trang_" + 0 + last);
         results.add(first + "tam_trang_" + 1 + last);
         return results;
+    }
+
+    public static ArrayList<String> getAllShownImagesPath(Activity activity) {
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        String absolutePathOfImage = null;
+        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+
+        cursor = activity.getContentResolver().query(uri, projection, null,
+                null, null);
+        ArrayList<String> listOfAllImages = new ArrayList<>();
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while (cursor.moveToNext()) {
+            absolutePathOfImage = cursor.getString(column_index_data);
+            listOfAllImages.add(absolutePathOfImage);
+        }
+        return listOfAllImages;
     }
 }
