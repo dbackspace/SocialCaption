5055bd8f328a81029aaa37b3e8cfcdef03eec5f1
binhnk08 <39753132+binhnk08@users.noreply.github.com>
2021-03-15 16:41:16 +0700

    Add files via upload

--- app/src/main/java/com/xlteam/socialcaption/ui/home/HomePageActivity.java
+++ app/src/main/java/com/xlteam/socialcaption/ui/home/HomePageActivity.java
@@ -4,14 +4,13 @@ import android.Manifest;
 import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,42 +38,37 @@ import com.xlteam.socialcaption.external.utility.utils.Utility;
 import com.xlteam.socialcaption.ui.edit.EditCaptionActivity;
 import com.xlteam.socialcaption.ui.home.created.PictureCreatedDialogFragment;
 import com.xlteam.socialcaption.ui.home.firebase.PictureFirebaseDialogFragment;
-import com.xlteam.socialcaption.ui.home.local.PictureLocalDialogFragment;
-
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
 
 public class HomePageActivity extends AppCompatActivity {
-    RecyclerView rvLocal, rvFirebase, rvCreated;
-    TextView tvViewMoreFirebase, tvViewMoreCreated, tvViewMoreLocal;
+    RecyclerView rvFirebase, rvCreated;
+    TextView tvViewMoreFirebase, tvViewMoreCreated;
     TextView tvEmptyCreated;
+    LinearLayout layoutTakePhoto, layoutGallery;
+
     private static final int REQUEST_CODE_PICK_PHOTO_HOME = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        rvLocal = findViewById(R.id.rv_picture_local);
         rvFirebase = findViewById(R.id.rv_picture_firebase);
         rvCreated = findViewById(R.id.rv_picture_created);
-        tvViewMoreLocal = findViewById(R.id.view_more_picture_local);
         tvViewMoreFirebase = findViewById(R.id.view_more_picture_firebase);
         tvViewMoreCreated = findViewById(R.id.view_more_picture_created);
         tvEmptyCreated = findViewById(R.id.tv_empty_picture_created);
+        layoutTakePhoto = findViewById(R.id.layout_take_photo);
+        layoutGallery = findViewById(R.id.layout_gallery);
 
         Dexter.withContext(this)
                 .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        rvLocal.setAdapter(new PictureHomeAdapter(HomePageActivity.this, 1, Utility.getAllShownImagesPath(HomePageActivity.this)));
                         rvCreated.setAdapter(new PictureHomeAdapter(HomePageActivity.this, 3, FileUtils.getListPathsIfFolderExist()));
                     }
 
                     @Override
                     public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-                        rvLocal.setAdapter(new PictureHomeAdapter(HomePageActivity.this, 1, new ArrayList<>()));
                     }
 
                     @Override
@@ -84,7 +78,6 @@ public class HomePageActivity extends AppCompatActivity {
                 }).check();
 //        rvGallery.setDrawingCacheEnabled(true);
 //        rvGallery.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
-        rvLocal.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
 
 //        rvUrl.setDrawingCacheEnabled(true);
 //        rvUrl.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
@@ -98,14 +91,6 @@ public class HomePageActivity extends AppCompatActivity {
             tvEmptyCreated.setVisibility(View.VISIBLE);
             rvCreated.setVisibility(View.GONE);
         }
-//
-        tvViewMoreLocal.setOnClickListener(view -> {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            ViManager.getInstance().setFragmentDefaultAnimation(HomePageActivity.this, fragmentTransaction);
-            PictureLocalDialogFragment pictureLocalDialogFragment = new PictureLocalDialogFragment();
-            pictureLocalDialogFragment.show(fragmentTransaction, "dialog_local");
-        });
 
         tvViewMoreFirebase.setOnClickListener(view -> {
             FragmentManager fragmentManager = getSupportFragmentManager();
@@ -122,6 +107,19 @@ public class HomePageActivity extends AppCompatActivity {
             PictureCreatedDialogFragment pictureCreatedDialogFragment = new PictureCreatedDialogFragment();
             pictureCreatedDialogFragment.show(fragmentTransaction, "dialog_created");
         });
+        layoutTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        layoutGallery.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 5);
+        });
     }
 
     @Override
@@ -133,10 +131,10 @@ public class HomePageActivity extends AppCompatActivity {
                     if (data == null) {
                         Toast.makeText(this, "Bạn chưa chọn ảnh.", Toast.LENGTH_SHORT).show();
                     } else {
-                        final String imageUriPath = data.getData().getPath();
                         Intent intent = new Intent(this, EditCaptionActivity.class);
-                        intent.putExtra("EXTRA_PICK_PHOTO_URL", imageUriPath);
-                        startActivityForResult(intent, REQUEST_CODE_PICK_PHOTO_HOME);
+                        intent.putExtra("EXTRA_PICK_PHOTO_URL", data.getData());
+                        intent.putExtra("EXTRA_TYPE_PICTURE", 0);
+                        startActivity(intent);
                     }
                 }
                 break;
