3a9df03c7432fba0d92960435eca666d756483e0
binhnk08 <39753132+binhnk08@users.noreply.github.com>
2021-03-17 17:37:57 +0700

    Add files via upload

--- app/src/main/java/com/xlteam/socialcaption/ui/home/HomePageActivity.java
+++ app/src/main/java/com/xlteam/socialcaption/ui/home/HomePageActivity.java
@@ -1,14 +1,13 @@
 package com.xlteam.socialcaption.ui.home;
 
 import android.Manifest;
-import android.app.Dialog;
-import android.content.ActivityNotFoundException;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.view.Window;
-import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,7 +15,6 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -28,7 +26,6 @@ import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
-import com.xlteam.socialcaption.BuildConfig;
 import com.xlteam.socialcaption.R;
 import com.xlteam.socialcaption.external.utility.animation.ViManager;
 import com.xlteam.socialcaption.external.utility.utils.Constant;
@@ -38,14 +35,14 @@ import com.xlteam.socialcaption.ui.edit.EditCaptionActivity;
 import com.xlteam.socialcaption.ui.home.created.PictureCreatedDialogFragment;
 import com.xlteam.socialcaption.ui.home.firebase.PictureFirebaseDialogFragment;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements DialogInterface.OnDismissListener {
     RecyclerView rvFirebase, rvCreated;
     TextView tvViewMoreFirebase, tvViewMoreCreated;
     TextView tvEmptyCreated;
     LinearLayout layoutTakePhoto, layoutGallery;
     ImageView imgSettings;
-
-    private static final int REQUEST_CODE_PICK_PHOTO_HOME = 10;
+    PictureFirebaseDialogFragment pictureFirebaseDialogFragment;
+    PictureCreatedDialogFragment pictureCreatedDialogFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +62,7 @@ public class HomePageActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        rvCreated.setAdapter(new PictureHomeAdapter(HomePageActivity.this, 3, FileUtils.getListPathsIfFolderExist()));
+                        rvCreated.setAdapter(new PictureHomeAdapter(HomePageActivity.this, Constant.TYPE_PICTURE_CREATED, FileUtils.getListPathsIfFolderExist()));
                     }
 
                     @Override
@@ -77,17 +74,13 @@ public class HomePageActivity extends AppCompatActivity {
                         permissionToken.continuePermissionRequest();
                     }
                 }).check();
-//        rvGallery.setDrawingCacheEnabled(true);
-//        rvGallery.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
-
-//        rvUrl.setDrawingCacheEnabled(true);
-//        rvUrl.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
         rvFirebase.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
-        rvFirebase.setAdapter(new PictureHomeAdapter(this, 2, Utility.getUrlPictureHome()));
+        rvFirebase.setAdapter(new PictureHomeAdapter(this, Constant.TYPE_PICTURE_FIREBASE, Utility.getUrlPictureHome()));
 
 //        rvCreated.setDrawingCacheEnabled(true);
 //        rvCreated.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
         rvCreated.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
+        rvCreated.setAdapter(new PictureHomeAdapter(HomePageActivity.this, Constant.TYPE_PICTURE_CREATED, FileUtils.getListPathsIfFolderExist()));
         if (FileUtils.getListPathsIfFolderExist().isEmpty()) {
             tvEmptyCreated.setVisibility(View.VISIBLE);
             rvCreated.setVisibility(View.GONE);
@@ -97,7 +90,7 @@ public class HomePageActivity extends AppCompatActivity {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             ViManager.getInstance().setFragmentDefaultAnimation(HomePageActivity.this, fragmentTransaction);
-            PictureFirebaseDialogFragment pictureFirebaseDialogFragment = new PictureFirebaseDialogFragment();
+            pictureFirebaseDialogFragment = new PictureFirebaseDialogFragment();
             pictureFirebaseDialogFragment.show(fragmentTransaction, "dialog_firebase");
         });
 
@@ -105,21 +98,19 @@ public class HomePageActivity extends AppCompatActivity {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             ViManager.getInstance().setFragmentDefaultAnimation(HomePageActivity.this, fragmentTransaction);
-            PictureCreatedDialogFragment pictureCreatedDialogFragment = new PictureCreatedDialogFragment();
+            pictureCreatedDialogFragment = new PictureCreatedDialogFragment();
             pictureCreatedDialogFragment.show(fragmentTransaction, "dialog_created");
         });
-        layoutTakePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
+        layoutTakePhoto.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, Constant.REQUEST_CODE_TAKE_PHOTO);
         });
 
         layoutGallery.setOnClickListener(v -> {
             Intent intent = new Intent();
             intent.setType("image/*");
             intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 5);
+            startActivityForResult(Intent.createChooser(intent, getString(R.string.select_picture)), Constant.REQUEST_CODE_PICK_PHOTO_GALLERY);
         });
 
         imgSettings.setOnClickListener(v -> {
@@ -134,70 +125,45 @@ public class HomePageActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case 5:
-                if (resultCode == RESULT_OK) {
-                    if (data == null) {
-                        Toast.makeText(this, "Bạn chưa chọn ảnh.", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Intent intent = new Intent(this, EditCaptionActivity.class);
-                        intent.putExtra("EXTRA_PICK_PHOTO_URL", data.getData());
-                        intent.putExtra("EXTRA_TYPE_PICTURE", 0);
-                        startActivity(intent);
-                    }
+        if (requestCode == Constant.REQUEST_CODE_PICK_PHOTO_GALLERY) {
+            if (resultCode == RESULT_OK) {
+                if (data == null) {
+                    Toast.makeText(this, R.string.not_selected_picture, Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(this, EditCaptionActivity.class);
+                    intent.putExtra(Constant.EXTRA_PICK_PHOTO_URL, data.getData());
+                    intent.putExtra(Constant.EXTRA_TYPE_PICTURE, Constant.TYPE_PICK_PHOTO);
+                    startActivityForResult(intent, Constant.REQUEST_CODE_PHOTO_FROM_HOME);
                 }
-                break;
+            }
+        } else if (requestCode == Constant.REQUEST_CODE_TAKE_PHOTO) {
+            Bitmap photo = null;
+            if (data != null) {
+                if (data.getExtras() != null)
+                    photo = (Bitmap) data.getExtras().get("data");
+            }
+            if (photo != null) {
+                Intent intent = new Intent(this, EditCaptionActivity.class);
+                intent.putExtra(Constant.EXTRA_PICK_PHOTO_URL, photo);
+                intent.putExtra(Constant.EXTRA_TYPE_PICTURE, Constant.TYPE_TAKE_PHOTO);
+                startActivityForResult(intent, Constant.REQUEST_CODE_PHOTO_FROM_HOME);
+            }
+        } else if (requestCode == Constant.REQUEST_CODE_PHOTO_FROM_HOME) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (pictureCreatedDialogFragment != null) {
+                    pictureCreatedDialogFragment.dismiss();
+                }
+                if (pictureFirebaseDialogFragment != null) {
+                    pictureFirebaseDialogFragment.dismiss();
+                }
+                rvCreated.setAdapter(new PictureHomeAdapter(HomePageActivity.this, Constant.TYPE_PICTURE_CREATED, FileUtils.getListPathsIfFolderExist()));
+            }
         }
     }
 
-    private void rateApp() {
-        Uri uri = Uri.parse(Constant.LINK_MARKET_APP + BuildConfig.APPLICATION_ID);
-        Intent intentMarket = new Intent(Intent.ACTION_VIEW, uri);
-        intentMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-        try {
-            startActivity(intentMarket);
-        } catch (ActivityNotFoundException e) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constant.LINK_GOOGLE_PLAY_APP + BuildConfig.APPLICATION_ID)));
-        }
-    }
-
-    private void recommendApp() {
-        Uri uri = Uri.parse(Constant.LINK_MARKET_DEVELOPER);
-        Intent intentMarket = new Intent(Intent.ACTION_VIEW, uri);
-        intentMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-        try {
-            startActivity(intentMarket);
-        } catch (ActivityNotFoundException e) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constant.LINK_GOOGLE_PLAY_DEVELOPER)));
-        }
-    }
-
-    private void shareApp() {
-        try {
-            Intent shareIntent = new Intent(Intent.ACTION_SEND);
-            shareIntent.setType("text/plain");
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name));
-            String shareMessage = Constant.LINK_GOOGLE_PLAY_APP + BuildConfig.APPLICATION_ID;
-            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
-            startActivity(Intent.createChooser(shareIntent, getString(R.string.choose_one)));
-        } catch (Exception e) {
-            //e.toString();
-        }
-    }
-
-    private void showDialogFullScreen(String assetName, int title) {
-        Dialog dialog = new Dialog(this, R.style.Theme_SocialCaption);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.dialog_web_view_fullscreen);
-        TextView tvTitle = dialog.findViewById(R.id.tvTitle);
-        tvTitle.setText(title);
-        WebView webView = dialog.findViewById(R.id.web);
-        webView.setBackgroundColor(ContextCompat.getColor(this, R.color.color_FC));
-        String urlWebView = "file:///android_asset/" + assetName;
-        webView.loadUrl(urlWebView);
-        ImageView imgBack = dialog.findViewById(R.id.imgBack);
-        imgBack.setOnClickListener(v -> dialog.dismiss());
-        dialog.show();
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        rvCreated.setAdapter(new PictureHomeAdapter(HomePageActivity.this, Constant.TYPE_PICTURE_CREATED, FileUtils.getListPathsIfFolderExist()));
     }
 
 //    @Override
--- app/src/main/java/com/xlteam/socialcaption/ui/home/PictureHomeAdapter.java
+++ app/src/main/java/com/xlteam/socialcaption/ui/home/PictureHomeAdapter.java
@@ -1,5 +1,6 @@
 package com.xlteam.socialcaption.ui.home;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -14,19 +15,20 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.xlteam.socialcaption.R;
+import com.xlteam.socialcaption.external.utility.utils.Constant;
 import com.xlteam.socialcaption.ui.edit.EditCaptionActivity;
 
 import java.util.List;
 
 import timber.log.Timber;
 
-public class PictureHomeAdapter extends RecyclerView.Adapter {
+public class PictureHomeAdapter extends RecyclerView.Adapter<PictureHomeAdapter.ViewHolder> {
     private final int mType;
     private Context mContext;
     private final List<String> mUrls;
     private final RequestOptions requestOptions;
 
-    public PictureHomeAdapter(Context context, int type, List<String> urls) {
+    PictureHomeAdapter(Context context, int type, List<String> urls) {
         mType = type;
         mUrls = urls;
         mContext = context;
@@ -36,70 +38,42 @@ public class PictureHomeAdapter extends RecyclerView.Adapter {
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if (viewType == 0) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_camera, parent, false);
-            return new CameraViewHolder(view);
-        } else {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_picture_home, parent, false);
-            return new ViewHolder(view);
-        }
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_picture_home, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-//        Picasso.get().load(Utility.getUrlByCategoryIndex(mNumberCategory, position)).into(holder.imgPicture);
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
         Timber.e("loading position: %d", position);
-        if (holder.getItemViewType() == 0) {
-            CameraViewHolder cameraViewHolder = (CameraViewHolder) holder;
-            cameraViewHolder.itemView.setOnClickListener(view -> {
-                //pick ảnh
-            });
-        } else {
-            ViewHolder viewHolder = (ViewHolder) holder;
-            String url = mUrls.get(position);
-            if (mType == 1) {
-                Glide.with(holder.itemView.getContext())
-                        .load("file://" + url)
+        String url = mUrls.get(position);
+        if (mType == Constant.TYPE_PICTURE_FIREBASE) {
+            Glide.with(mContext)
+                    .load(url)
+                    .error(R.drawable.ic_camera)
 //                        .apply(requestOptions.override(600, 600))
-                        .error(R.drawable.ic_camera)
-                        .fitCenter()
-                        .into(viewHolder.imgPicture);
-            } else if (mType == 2) {
-                Glide.with(holder.itemView.getContext())
-                        .load(url)
-                        .error(R.drawable.ic_camera)
+                    .into(viewHolder.imgPicture);
+        } else if (mType == Constant.TYPE_PICTURE_CREATED) {
+            Glide.with(mContext)
+                    .load("file://" + url)
 //                        .apply(requestOptions.override(600, 600))
-                        .into(viewHolder.imgPicture);
-            } else if (mType == 3) {
-                Glide.with(holder.itemView.getContext())
-                        .load("file://" + url)
-//                        .apply(requestOptions.override(600, 600))
-                        .fitCenter()
-                        .into(viewHolder.imgPicture);
-            }
-            viewHolder.itemView.setOnClickListener(view -> {
-                Intent intent = new Intent(mContext, EditCaptionActivity.class);
-                intent.putExtra("EXTRA_URL_PICTURE", url);
-                intent.putExtra("EXTRA_TYPE_PICTURE", mType);
-                mContext.startActivity(intent);
-            });
+                    .fitCenter()
+                    .into(viewHolder.imgPicture);
         }
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        if (mType == 1 && position == 0) return 0;
-        return 1;
+        viewHolder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(mContext, EditCaptionActivity.class);
+            intent.putExtra(Constant.EXTRA_URL_PICTURE, url);
+            intent.putExtra(Constant.EXTRA_TYPE_PICTURE, mType);
+            ((Activity) mContext).startActivityForResult(intent, Constant.REQUEST_CODE_PHOTO_FROM_HOME);
+        });
     }
 
     @Override
     public int getItemCount() {
-        if (mType == 1 && mUrls.size() == 0) return 1;
-        return Math.min(mUrls.size(), 10);
+        return mUrls.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    static class ViewHolder extends RecyclerView.ViewHolder {
         private final ImageView imgPicture;
 
         ViewHolder(@NonNull View itemView) {
@@ -107,14 +81,5 @@ public class PictureHomeAdapter extends RecyclerView.Adapter {
             imgPicture = itemView.findViewById(R.id.img_picture);
         }
 
-    }
-
-    static class CameraViewHolder extends RecyclerView.ViewHolder {
-        View itemView;
-
-        CameraViewHolder(@NonNull View itemView) {
-            super(itemView);
-            this.itemView = itemView;
-        }
     }
 }
