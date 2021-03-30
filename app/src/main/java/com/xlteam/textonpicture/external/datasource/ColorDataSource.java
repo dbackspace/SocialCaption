2a7ffcc35c40b20808e636662e3eb6718d02f7af
binhnk08 <39753132+binhnk08@users.noreply.github.com>
2021-03-30 17:06:09 +0700

    Add files via upload

--- app/src/main/java/com/xlteam/textonpicture/external/datasource/ColorDataSource.java
+++ app/src/main/java/com/xlteam/textonpicture/external/datasource/ColorDataSource.java
@@ -20,14 +20,33 @@ public class ColorDataSource {
     private void createData() {
         colors.add("000000"); // black
         colors.add("FFFFFF"); // white
-        colors.add("0000FF"); // blue
-        colors.add("00FFFF"); // cyan, aqua
-        colors.add("00FF00"); // green
-        colors.add("FFFF00"); // yellow
+        colors.add("A9A9A9");
+        colors.add("E0E0E0");
+        colors.add("CD853F");
         colors.add("FF0000"); // red
-        colors.add("FF6633"); // orange
-        colors.add("800080"); // purple
-        colors.add("FF00FF"); // magenta
+        colors.add("ff4000");
+        colors.add("ff8000");
+        colors.add("ffbf00");
+        colors.add("ffff00");
+        colors.add("bfff00");
+        colors.add("80ff00");
+        colors.add("40ff00");
+        colors.add("00ff00");
+        colors.add("00ff40");
+        colors.add("00ff80");
+        colors.add("00ffbf");
+        colors.add("00ffff");
+        colors.add("00bfff");
+        colors.add("0080ff");
+        colors.add("0040ff");
+        colors.add("0000ff");
+        colors.add("4000ff");
+        colors.add("8000ff");
+        colors.add("bf00ff");
+        colors.add("ff00ff");
+        colors.add("ff00bf");
+        colors.add("ff0080");
+        colors.add("ff0040");
     }
 
     public ArrayList<String> getAllData() {
