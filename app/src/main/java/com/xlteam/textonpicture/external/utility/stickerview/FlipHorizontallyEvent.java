e3b251cd31536a4e113cd780c5e57cb72a6b1a7a
Duc Nguyen <dbackspace.hust@gmail.com>
2021-04-14 16:19:00 +0700

    Remove unused code

--- app/src/main/java/com/xlteam/textonpicture/external/utility/stickerview/FlipHorizontallyEvent.java
+++
@@ -1,9 +0,0 @@
-package com.xlteam.textonpicture.external.utility.stickerview;
-
-public class FlipHorizontallyEvent extends AbstractFlipEvent {
-    @Override
-    @StickerView.Flip
-    protected int getFlipDirection() {
-        return StickerView.FLIP_HORIZONTALLY;
-    }
-}
--- app/src/main/java/com/xlteam/textonpicture/external/utility/stickerview/FlipVerticallyEvent.java
+++
@@ -1,9 +0,0 @@
-package com.xlteam.textonpicture.external.utility.stickerview;
-
-public class FlipVerticallyEvent extends AbstractFlipEvent {
-    @Override
-    @StickerView.Flip
-    protected int getFlipDirection() {
-        return StickerView.FLIP_VERTICALLY;
-    }
-}
